6d03b6f9bde46159cb86db8a8a3c3cdf92a9b47e
Syeda Aleeza Abbas <166072392+syedaaleezaabbas@users.noreply.github.com>
2024-04-07 13:01:02 +0500

    change in code

--- index.ts
+++ index.ts
@@ -11,14 +11,14 @@ const randomNumber = Math.floor(Math.random() * 10 + 1);
 
 
  const answers = await inquirer.prompt([
-     {
-         name: "userGuessedNumber",
-         type: "number",
-         message: "Please guess a number between 1-10: ",
-     },
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10:",
+    }
  ]);
-
- if(answers.userGuessedNumber === randomNumber){
+     
+  if(answers.userGuessedNumber === randomNumber){
      console.log("Congratulations! you guessed right number.")
  }else {
      console.log("You guessed wrong number");
